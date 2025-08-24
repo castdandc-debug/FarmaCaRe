589f3a99e3fad430285804bf8f790cc9cd733fd3
Danny Castillo <dannycastillo@MacBookAir.mshome.net>
2025-08-24 07:00:45 -0600

    Corrección: agregar is_authenticated a Usuario

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
-# La instancia `db` viene de `app/__init__.py`
+# La instancia db viene de app/__init__.py
 from app import db
 
-class Usuario(db.Model):
+class Usuario(db.Model, UserMixin):
     __tablename__ = 'usuarios'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -14,29 +16,25 @@ class Usuario(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now)
 
     def set_password(self, password):
-        from werkzeug.security import generate_password_hash
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
-        from werkzeug.security import check_password_hash
         return check_password_hash(self.password, password)
+
+    def get_id(self):
+        return str(self.id)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
 
     def __repr__(self):
         return f'<Usuario {self.username}>'
-
-class Medicamento(db.Model):
-    __tablename__ = 'medicamentos'
-
-    id = db.Column(db.Integer, primary_key=True)
-    codigo_barras = db.Column(db.String(50), unique=True, nullable=False)
-    nombre_comercial = db.Column(db.String(100), nullable=False)
-    nombre_generico = db.Column(db.String(100))
-    laboratorio = db.Column(db.String(100), nullable=False)
-    presentacion = db.Column(db.String(100), nullable=False)
-    grupo = db.Column(db.String(20), nullable=False)
-    iva = db.Column(db.Float, default=0.0)
-    precio_venta = db.Column(db.Float, nullable=False)
-    stock = db.Column(db.Integer, default=0)
-
-    def __repr__(self):
-        return f'<Medicamento {self.nombre_comercial}>'
