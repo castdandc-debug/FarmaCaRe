4f743000abae464a039b2aac575ebe6bb3cb4ffa
Danny Castillo <dannycastillo@MacBookAir.mshome.net>
2025-08-30 06:59:23 -0600

    Corregida la lógica de redirección en la ruta de login para evitar bucle

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,34 +1,33 @@
 # app/views/auth.py
-
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import check_password_hash
 from app.models import Usuario
+from app.forms import LoginForm
 
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('auth_bp', __name__)
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    # Si el usuario ya está autenticado, redirigir directamente al dashboard
+    # Si el usuario ya está autenticado, lo enviamos directamente al dashboard.
     if current_user.is_authenticated:
         return redirect(url_for('main.dashboard'))
 
-    if request.method == 'POST':
-        nombre_usuario = request.form['username']
-        contraseña = request.form['password']
-        usuario = Usuario.query.filter_by(nombre=nombre_usuario).first()
-
-        if usuario and check_password_hash(usuario.contraseña, contraseña):
-            login_user(usuario)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Usuario.query.filter_by(usuario=form.username.data).first()
+        if user and check_password_hash(user.clave_hash, form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            # Redirige al usuario al dashboard después de un inicio de sesión exitoso.
             return redirect(url_for('main.dashboard'))
         else:
-            flash('Nombre de usuario o contraseña incorrectos.')
-            return render_template('auth/login.html')
+            flash('Usuario o contraseña inválidos.')
 
-    return render_template('auth/login.html')
+    return render_template('login.html', form=form)
+
 
 @auth_bp.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('auth_bp.login'))
